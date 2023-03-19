52c43871ba161f35f3c82510f7329bb525edb616
BanPei Lim <banpei.lim@gmail.com>
2023-03-19 21:54:26 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,8 @@
+const data = {
+  categories: [
+    { name: "Beverages", image: "../images/beverages.jpg" },
+    { name: "Breakfast", image: "../images/breakfast.jpg" },
+    { name: "Burgers", image: "../images/burgers.jpg" },
+  ],
+};
+module.exports = data;
