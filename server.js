a7c3d9a43b118bc29832fcf984d505f2d92a834f
BanPei Lim <banpei.lim@gmail.com>
2023-03-19 22:57:29 +0800

    third

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const data = require("./data");
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // allow all origins
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/api/categories", (req, res) => res.send(data.categories));
 
 const port = process.env.PORT || 5000;
