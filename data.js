e4b5f21bdfb2f9a31d4cc9e14f05f27c5eb132d6
BanPei Lim <banpei.lim@gmail.com>
2023-03-21 22:15:35 +0800

    fourth

--- data.js
+++ data.js
@@ -1,8 +1,14 @@
 const data = {
   categories: [
-    { name: "Beverages", image: "../images/beverages.jpg" },
-    { name: "Breakfast", image: "../images/breakfast.jpg" },
-    { name: "Burgers", image: "../images/burgers.jpg" },
+    {
+      name: "Beverages",
+      image: "https://imgtr.ee/images/2023/03/21/qfhB1.jpg",
+    },
+    {
+      name: "Breakfast",
+      image: "https://imgtr.ee/images/2023/03/21/qfCRX.jpg",
+    },
+    { name: "Burgers", image: "https://imgtr.ee/images/2023/03/21/qfIpV.jpg" },
   ],
 };
 module.exports = data;
