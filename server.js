10d3ba353f008e12877d27dfa672e163a6be7f68
BanPei Lim <banpei.lim@gmail.com>
2023-03-22 21:29:29 +0800

    fifth

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const data = require("./data");
+require("dotenv").config();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to database", err);
+  });
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // allow all origins
@@ -10,6 +27,36 @@ app.use(function (req, res, next) {
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
+});
+
+const Product = mongoose.model(
+  "products",
+  new mongoose.Schema({
+    name: String,
+    description: String,
+    image: String,
+    price: Number,
+    calorie: Number,
+    category: String,
+  })
+);
+
+app.get("/api/products/seed", async (req, res) => {
+  // await Product.remove({});
+  const products = await Product.insertMany(data.products);
+  res.send({ products });
+});
+
+app.get("/api/products", async (req, res) => {
+  const { category } = req.query;
+  const products = await Product.find(category ? { category } : {});
+  res.send(products);
+});
+
+app.post("/api/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  const savedProduct = await newProduct.save();
+  res.send(savedProduct);
 });
 
 app.get("/api/categories", (req, res) => res.send(data.categories));
